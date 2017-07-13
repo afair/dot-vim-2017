f25c658f727447c86a6dff2a94e03e4df89b9b67
Allen Fair <allen.fair@gmail.com>
2017-07-13 10:19:49 -0400

    Adds alias.sh to source alias commands to setup.

---
+++ alias.sh
@@ -0,0 +1,10 @@
+################################################################################
+# This file contains bash/zsh alias commands.  Source it to setup these alias'.
+#
+#     bash> source ~/.vim2/alias.sh
+#
+################################################################################
+
+alias vim2='vim -u ~/.vim2/vimrc'
+alias mvim2='mvim -u ~/.vim2/vimrc -U ~/.vim2/gvimrc'
+alias gvim2='gvim -u ~/.vim2/vimrc -U ~/.vim2/gvimrc'
